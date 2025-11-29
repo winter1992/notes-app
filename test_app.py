448c5968d105a671c0db77271d614e33c009fb03
Ayumu_Osaka_Kasuga <artyom.syulin@mail.ru>
2025-11-30 01:14:11 +0400

    Fix security: remove binary files and debug mode

--- test_app.py
+++
@@ -1,9 +0,0 @@
-def test_debug_enabled():
-    \"\"\"Этот тест должен падать если debug=True\"\"\"
-    debug_mode = False  # Измените на True для демонстрации ошибки
-    assert debug_mode == False, "Debug mode should be disabled in production"
-
-def test_ssl_required():
-    \"\"\"Проверка что SSL требуется\"\"\"
-    ssl_enabled = True
-    assert ssl_enabled == True, "SSL should be enabled"
